8d7ca6ea3ae22d36c4b05f1e83baba6f32a38ba7
ruffish <AlexBowgen.Personal@Gmail.com>
2022-12-08 23:36:05 +0000

    test7

--- Mimic/mimicGameLogic.js
+++ Mimic/mimicGameLogic.js
@@ -117,8 +117,6 @@ class MimicGame {
 
         // Assign each player a role
         for (let playerId in players) {
-            console.log(roles.pop());
-            console.log("roles variable below:");
             console.log(roles);
             players[playerId]['role'] = roles.pop();
         }
