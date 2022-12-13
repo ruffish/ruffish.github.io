c62795cb135bc154ffed2f761fd76743e000fd35
ruffish <AlexBowgen.Personal@Gmail.com>
2022-12-13 20:36:54 +0000

    test 1

--- Mimic/mimicGameLogic.js
+++ Mimic/mimicGameLogic.js
@@ -440,6 +440,8 @@ class MimicGame {
     eliminatePlayer(playerID) {
         // Remove the player from the playersInGame dictionary
         delete playersInGame[playerID];
+        conslole.log(playersInGame);
+        console.log("eliminatePlayer activated");
         // Announce the elimination of the player
         triggerAnnounceElimination(playerID);
         for (let step = 0; step < conn.length; step++) {
