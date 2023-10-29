159050651ebddf5ed6ffb1e6797e5c11e9db2f7f
ruffish <AlexBowgen.Personal@Gmail.com>
2023-10-29 11:22:25 +0000

    b

--- CaptionIt/captionitlogic.js
+++ CaptionIt/captionitlogic.js
@@ -2,14 +2,13 @@
 
 const game = {
     state: 'lobby', // Possible states: 'lobby', 'caption', 'vote', 'results'
-    players: players, // Store player information
     pictureURL: '',
     captions: [], // Store submitted captions
     votes: [], // Store votes for captions
 
     // Add a new player to the game
-    addPlayer: function(playerID, playerName) {
-        this.players[playerID] = {
+    addPlayer: function (playerID, playerName) {
+        players[playerID] = {
             id: playerID,
             name: playerName,
             score: 0
@@ -18,13 +17,13 @@ const game = {
     },
 
     // Generate a new picture URL
-    generatePicture: function() {
+    generatePicture: function () {
         // Your logic to generate a new picture URL
         this.pictureURL = 'https://example.com/new-picture.jpg';
     },
 
     // Submit a caption for the current picture
-    submitCaption: function(playerID, captionText) {
+    submitCaption: function (playerID, captionText) {
         this.captions.push({
             playerID: playerID,
             text: captionText
@@ -33,20 +32,37 @@ const game = {
     },
 
     // Submit a vote for a specific caption
-    submitVote: function(playerID, votedCaptionIndex) {
+    submitVote: function (playerID, votedCaptionIndex) {
         this.votes[playerID] = votedCaptionIndex;
+
         // Check if all players have voted and update the game state
+        let allPlayersVoted = true;
+        for (let id in players) {
+            if (players[id].inPlay && !this.votes.hasOwnProperty(id)) {
+                allPlayersVoted = false;
+                break;
+            }
+        }
+
+        if (allPlayersVoted) {
+            // Reset each players ready status
+            for (let id in players) {
+                players[id].ready = false;
+            }
+            // Move to the next state (e.g., from vote to results)
+            this.nextState();
+        }
     },
 
     // Calculate and update scores based on the votes
-    updateScores: function() {
+    updateScores: function () {
         // Your logic to calculate scores based on the votes
         // Update the scores in the players object
         // Update the UI with the new scores
     },
 
     // Move the game to the next stage (e.g., from caption to vote)
-    nextState: function() {
+    nextState: function () {
         switch (this.state) {
             case 'lobby':
                 this.state = 'caption';
@@ -65,5 +81,21 @@ const game = {
                 break;
         }
         // Update the UI based on the new game state
+    },
+
+    resetGame: function () {
+        // Reset the game state
+        this.state = 'lobby';
+        this.pictureURL = '';
+        this.captions = [];
+        this.votes = [];
+
+        // Reset the players
+        for (let playerID in players) {
+            players[playerID].score = 0;
+        }
+
+        // Update the UI
+
     }
 };
---
+++ PAAL-lost-rewards-calculator/PAAL-lost-rewards-calculator.js
@@ -0,0 +1,211 @@
+let ETH_API_KEY = 'TCZMYYGC5XWCVK6SCA482E9IJ49PK2HMKE';
+const ETH_ENDPOINT = 'https://api.etherscan.io/api';
+const CLAIMED_CONTRACT_ADDRESS = '0xb04Af4843e3808e1FE6F7A8ee4F456937D7Afae8';
+const STAKING_CONTRACT_ADDRESS = '0x8431060c8e72793aFaDA261E9DD0Ab950e80894F';
+let WALLET_ADDRESS = '0x0';
+
+const getContractAbi = async (contractAddress) => {
+    const params = {
+        module: 'contract',
+        action: 'getabi',
+        address: contractAddress,
+        apikey: ETH_API_KEY
+    };
+
+    try {
+        const response = await axios.get(ETH_ENDPOINT, {
+            params
+        });
+        const abiString = response.data.result;
+        return JSON.parse(abiString);
+    } catch (error) {
+        console.error("Failed to decode ABI", error);
+        return null;
+    }
+}
+
+const getUnstakeMethodId = (abi) => {
+    for (let item of abi) {
+        if (item.type === 'function' && item.name === 'unstake') {
+            const signature = `${item.name}(${item.inputs.map(input => input.type).join(',')})`;
+            const hashed = Web3.utils.keccak256(signature);
+            return hashed.slice(0, 10);
+        }
+    }
+    return null;
+}
+
+const getStakeMethodId = (abi) => {
+    for (let item of abi) {
+        if (item.type === 'function' && item.name === 'stake') {
+            const signature = `${item.name}(${item.inputs.map(input => input.type).join(',')})`;
+            const hashed = Web3.utils.keccak256(signature);
+            console.log(hashed.slice(0, 10));
+            return hashed.slice(0, 10);
+        }
+    }
+    return null;
+}
+
+const getClaimRewardsMethodId = (abi) => {
+    for (let item of abi) {
+        if (item.type === 'function' && item.name === 'claimRewards') {
+            const signature = `${item.name}(${item.inputs.map(input => input.type).join(',')})`;
+            const hashed = Web3.utils.keccak256(signature);
+            console.log(hashed.slice(0, 10))
+            return hashed.slice(0, 10);
+        }
+    }
+    return null;
+}
+
+const getLastClaimRewardsTimestampBeforeDate = async (walletAddress, methodId, endDateStr) => {
+    const params = {
+        module: 'account',
+        action: 'txlist',
+        address: walletAddress,
+        startblock: 0,
+        endblock: 99999999,
+        sort: 'desc',
+        apikey: ETH_API_KEY
+    };
+
+    console.log('after Wallet Address: ' + walletAddress);
+
+    try {
+        const response = await axios.get(ETH_ENDPOINT, {
+            params
+        });
+        console.log(response.data);
+        const transactions = response.data.result;
+
+        const endDate = new Date(endDateStr);
+        const endTimestamp = Math.floor(endDate.getTime() / 1000);
+
+        for (let tx of transactions) {
+            const txTimestamp = parseInt(tx.timeStamp);
+            console.log(tx)
+            if (tx.methodId === methodId && txTimestamp < endTimestamp) {
+                return new Date(txTimestamp * 1000).toISOString();
+            }
+        }
+    } catch (error) {
+        console.error("Failed to get transactions", error);
+        return null;
+    }
+}
+
+const calculateStakedAmountFromTransactions = (transactions, stakeMethodId, unstakeMethodId) => {
+    let stakedAmount = 0;
+    for (let tx of transactions) {
+        const encodedAmount = tx.input.slice(10);
+        const decodedAmount = parseInt(encodedAmount, 16) / Math.pow(10, 9);
+
+        if (tx.methodId === stakeMethodId) {
+            stakedAmount += decodedAmount;
+        } else if (tx.methodId === unstakeMethodId) {
+            stakedAmount -= decodedAmount;
+        }
+    }
+    return stakedAmount;
+}
+
+const calculateEarnings = (initialBalance, apr, startDate, endDate) => {
+    const timeDifferenceMinutes = (endDate - startDate) / 1000 / 60; // Convert time difference to minutes
+    const totalApr = (apr / 100) * timeDifferenceMinutes / (365 * 24 * 60); // Convert APR to per-minute basis
+    const earnings = initialBalance * totalApr;
+    return earnings;
+}
+
+const getTransactionsUpToDate = async (walletAddress, targetDate) => {
+    const targetTimestamp = Math.floor(targetDate.getTime() / 1000);
+
+    const params = {
+        module: 'account',
+        action: 'txlist',
+        address: walletAddress,
+        startblock: 0,
+        endblock: 99999999,
+        sort: 'desc',
+        apikey: ETH_API_KEY
+    };
+
+    try {
+        const response = await axios.get(ETH_ENDPOINT, {
+            params
+        });
+        const allTransactions = response.data.result;
+
+        // Filter transactions up to the given date
+        const filteredTransactions = allTransactions.filter(tx => parseInt(tx.timeStamp) <= targetTimestamp);
+        return filteredTransactions;
+    } catch (error) {
+        console.error("Failed to get transactions", error);
+        return [];
+    }
+}
+
+
+const main = async () => {
+    const endDateStr = '2023-10-27 17:43:00';
+
+    let abi = await getContractAbi(CLAIMED_CONTRACT_ADDRESS);
+    if (!abi) {
+        console.error("Unable to fetch the ABI for the claimed contract.");
+        return;
+    }
+
+    const claimRewardsMethodId = getClaimRewardsMethodId(abi);
+    if (!claimRewardsMethodId) {
+        console.error("Unable to fetch the method ID for the 'claimRewards' function.");
+        return;
+    }
+
+    abi = await getContractAbi(STAKING_CONTRACT_ADDRESS);
+    if (!abi) {
+        console.error("Unable to fetch the ABI for the staking contract.");
+        return;
+    }
+
+    const unstakeMethodId = getUnstakeMethodId(abi);
+    if (!unstakeMethodId) {
+        console.error("Unable to fetch the method ID for the 'unstake' function.");
+        return;
+    }
+
+    const stakeMethodId = getStakeMethodId(abi);
+    if (!stakeMethodId) {
+        console.error("Unable to fetch the method ID for the 'stake' function.");
+        return;
+    }
+
+    const lastClaimRewardsTimestamp = await getLastClaimRewardsTimestampBeforeDate(WALLET_ADDRESS, claimRewardsMethodId, endDateStr);
+    if (!lastClaimRewardsTimestamp) {
+        console.error("Unable to find the last claimRewards transaction.");
+        return;
+    }
+
+    const startDate = new Date(lastClaimRewardsTimestamp);
+    const endDate = new Date(endDateStr);
+
+    const transactions = await getTransactionsUpToDate(WALLET_ADDRESS, startDate);
+    const stakedAmountStart = calculateStakedAmountFromTransactions(transactions, stakeMethodId, unstakeMethodId);
+
+    const totalEarnedOldApr = calculateEarnings(stakedAmountStart, old_apr, startDate, endDate);
+    const totalEarnedNewApr = calculateEarnings(stakedAmountStart, new_apr, startDate, endDate);
+    const lostTokens = totalEarnedOldApr - totalEarnedNewApr;
+
+    const resultText = `
+        Staked amount: <br><span style="color: purple;">${stakedAmountStart.toFixed(4)} PAAL\n</span><br><br>
+        Total earned on old APR: <br><span style="color: purple;">${totalEarnedOldApr.toFixed(4)} PAAL\n</span><br><br>
+        Total earned on new APR: <br><span style="color: purple;">${totalEarnedNewApr.toFixed(4)} PAAL\n</span><br><br>
+        Lost tokens: <br><span style="color: purple;">${lostTokens.toFixed(4)} PAAL</span>
+    `;
+
+    // Update modal content
+    document.getElementById('modalResultText').innerHTML = resultText;
+}
+
+// Remember to define old_apr and new_apr before calling the main function:
+const old_apr = 105;
+const new_apr = 60;
